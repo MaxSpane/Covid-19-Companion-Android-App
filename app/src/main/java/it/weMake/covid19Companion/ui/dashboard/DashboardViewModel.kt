fee9c20a53005cc095a77ade195e4b4b39bcd3d8
max_spane <mohammedadetunji3@gmail.com>
2020-03-28 07:54:51 +0100

    First Commit

---
+++ app/src/main/java/it/weMake/covid19Companion/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package it.weMake.covid19Companion.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/it/weMake/covid19Companion/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package it.weMake.covid19Companion.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
