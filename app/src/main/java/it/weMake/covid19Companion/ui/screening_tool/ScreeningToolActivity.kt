3fc635962052e5cc954b83150820e614504317b4
max_spane <mohammedadetunji3@gmail.com>
2020-05-02 16:15:38 +0100

    Added ScreenToolActivity with navigation

---
+++ app/src/main/java/it/weMake/covid19Companion/ui/screening_tool/ScreeningToolActivity.kt
@@ -0,0 +1,13 @@
+package it.weMake.covid19Companion.ui.screening_tool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.weMake.covid19Companion.R
+
+class ScreeningToolActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screening_tool)
+    }
+}
---
+++ app/src/main/java/it/weMake/covid19Companion/ui/screening_tool/fragments/IntroFragment.kt
@@ -0,0 +1,21 @@
+package it.weMake.covid19Companion.ui.screening_tool.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import it.weMake.covid19Companion.R
+
+class IntroFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro, container, false)
+    }
+
+}
