a96aee249bd3831e3c73693d459ea9606068a739
max_spane <mohammedadetunji3@gmail.com>
2020-05-02 16:34:53 +0100

    UI for IntroScreeningFragment

--- app/src/main/java/it/weMake/covid19Companion/ui/screening_tool/fragments/ScreeningToolIntroFragment.kt
+++ app/src/main/java/it/weMake/covid19Companion/ui/screening_tool/fragments/ScreeningToolIntroFragment.kt
@@ -8,14 +8,14 @@ import android.view.ViewGroup
 
 import it.weMake.covid19Companion.R
 
-class IntroFragment : Fragment() {
+class ScreeningToolIntroFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro, container, false)
+        return inflater.inflate(R.layout.fragment_screening_tool_intro, container, false)
     }
 
 }
