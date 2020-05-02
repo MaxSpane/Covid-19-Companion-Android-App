c9d66b509f56d1c932c7724e3d38b1b94a349122
max_spane <mohammedadetunji3@gmail.com>
2020-05-02 11:25:53 +0100

    Fixed bug in CountryCasesViewholder

--- app/src/main/java/it/weMake/covid19Companion/ui/landing/dashboard/adapters/viewHolders/CountryCaseHolder.kt
+++ app/src/main/java/it/weMake/covid19Companion/ui/landing/dashboard/adapters/viewHolders/CountryCaseHolder.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import it.weMake.covid19Companion.R
 import it.weMake.covid19Companion.databinding.ItemCountryCasesSummaryBinding
 import it.weMake.covid19Companion.models.CountryCasesData
+import it.weMake.covid19Companion.utils.makeDisappear
 import it.weMake.covid19Companion.utils.numberWithCommas
 import it.weMake.covid19Companion.utils.show
 
@@ -36,20 +37,26 @@ class CountryCaseHolder(private val binding: ItemCountryCasesSummaryBinding): Re
         binding.confirmedValueTV.text = if(item.totalConfirmed == null){"Unknown"}else{item.totalConfirmed.numberWithCommas()}
         if (item.totalConfirmedDelta != null && item.totalConfirmedDelta != 0){
             binding.confirmedDeltaCP.show()
-
-            binding.confirmedDeltaCP.text = context.getString(R.string.new_cases_placeholder, item.totalConfirmedDelta.numberWithCommas())
+            val text = context.getString(R.string.new_cases_placeholder, item.totalConfirmedDelta.numberWithCommas())
+            binding.confirmedDeltaCP.text = text
+        }else{
+            binding.confirmedDeltaCP.makeDisappear()
         }
 
         binding.recoveredValueTV.text = if(item.totalRecovered == null){"Unknown"}else{item.totalRecovered.numberWithCommas()}
         if (item.totalRecoveredDelta != null && item.totalRecoveredDelta != 0){
             binding.recoveredDeltaCP.show()
             binding.recoveredDeltaCP.text = context.getString(R.string.new_cases_placeholder, item.totalRecoveredDelta.numberWithCommas())
+        }else{
+            binding.recoveredDeltaCP.makeDisappear()
         }
 
         binding.deathsValueTV.text = if(item.totalDeaths == null){"Unknown"}else{item.totalDeaths.numberWithCommas()}
         if (item.totalDeathsDelta != null && item.totalDeathsDelta != 0){
             binding.deathsDeltaCP.show()
             binding.deathsDeltaCP.text = context.getString(R.string.new_cases_placeholder, item.totalDeathsDelta.numberWithCommas())
+        }else{
+            binding.deathsDeltaCP.makeDisappear()
         }
 
     }
