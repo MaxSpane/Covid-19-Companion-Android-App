95f8a6540178c8c5864b4b3bd6cd52ddaddaa800
max_spane <mohammedadetunji3@gmail.com>
2020-05-04 01:16:23 +0100

    UI for QuestionFragment

--- app/src/main/java/it/weMake/covid19Companion/ui/screeningTool/fragments/InitialQuestionsFragment.kt
+++ app/src/main/java/it/weMake/covid19Companion/ui/screeningTool/fragments/InitialQuestionsFragment.kt
@@ -5,20 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import it.weMake.covid19Companion.R
+import it.weMake.covid19Companion.databinding.FragmentInitialQuestionsBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class InitialQuestionsFragment : Fragment() {
+class InitialQuestionsFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var binding: FragmentInitialQuestionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_initial_questions, container, false)
+        binding = FragmentInitialQuestionsBinding.inflate(inflater, container, false)
+
+        binding.nextMB.setOnClickListener(this)
+        return binding.root
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+
+            R.id.nextMB -> findNavController().navigate(R.id.action_initialQuestionsFragment_to_questionFragment)
+
+        }
     }
 
 }
