e269640d23ba14f48783a1c53975b5f39992f1e8
max_spane <mohammedadetunji3@gmail.com>
2020-05-30 16:24:21 +0100

    Added firebase cloud messaging

---
+++ app/src/main/java/it/weMake/covid19Companion/services/MyFirebaseMessagingService.kt
@@ -0,0 +1,30 @@
+package it.weMake.covid19Companion.services
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val TAG = "FirebaseMsgService"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
+        Log.d(TAG, "From: " + remoteMessage.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.size > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+}
