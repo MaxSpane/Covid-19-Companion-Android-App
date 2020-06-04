1fd21947b868673f13667ba88278d6284f617e17
max_spane <mohammedadetunji3@gmail.com>
2020-06-04 13:03:37 +0100

    Added icons for prevention tips

--- app/src/main/java/it/weMake/covid19Companion/utils/Utils.kt
+++ app/src/main/java/it/weMake/covid19Companion/utils/Utils.kt
@@ -3,8 +3,11 @@ package it.weMake.covid19Companion.utils
 import android.content.Context
 
 fun getFlagResourceId(context: Context, iso2: String): Int? =
+    getDrawableResourceId(context, "flag_${iso2.toLowerCase()}")
+
+fun getDrawableResourceId(context: Context, resourceName: String): Int? =
     try {
-        context.resources.getIdentifier("flag_${iso2.toLowerCase()}", "drawable", context.packageName)
+        context.resources.getIdentifier(resourceName, "drawable", context.packageName)
     }catch (e: Exception){
         null
     }
